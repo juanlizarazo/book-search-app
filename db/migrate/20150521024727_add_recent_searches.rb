6654cf444a893d0e2fa4ee6811ad792220779490
Juan Lizarazo <gabriellg4@gmail.com>
2015-05-20 20:52:34 -0600

    Add migration - recent_searches table

---
+++ db/migrate/20150521024727_add_recent_searches.rb
@@ -0,0 +1,16 @@
+class AddRecentSearches < ActiveRecord::Migration
+  def up
+    create_table :recent_searches do |t|
+      t.string :isbn
+      t.string :book_name
+      t.string :thumbnail_url
+      t.string :author
+      
+      t.timestamps null: false
+    end
+  end
+
+  def down
+    drop_table :recent_searches
+  end
+end
