43ac658413cd88b9dac2a42f4fd03eed41bccd73
Juan Lizarazo <gabriellg4@gmail.com>
2015-05-20 22:49:49 -0600

    Improve search, add flash error message

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,21 +1,31 @@
 class SearchController < ApplicationController
-  def index
-    isbn = params[:isbn]
+  def create
+    flash[:error] = "isbn \"#{params[:isbn]}\" not found" unless get_book_info(params[:isbn])
+    redirect_to root_url
+  end
 
+  def show
+    render json: get_book_info(params[:isbn]), status: :ok
+  end
+
+  private
+
+  def get_book_info(isbn)
     # Get book data from google's api
     book = Book.get("volumes?q=isbn:#{isbn}")
-    book_data = book.items.first[:volumeInfo]
 
-    # create entry in recent_searches
-    recent_search = RecentSearch.create(
-      {
-         isbn: isbn,
-         book_name: book_data[:title],
-         author: book_data[:authors].first,
-         thumbnail_url: book_data[:imageLinks][:smallThumbnail]
-      }
-    )
+    if book.items?
+      book_data = book.items.first[:volumeInfo]
 
-    render json: recent_search, status: :ok
+      # create entry in recent_searches and return result
+      RecentSearch.create(
+        {
+          isbn: isbn,
+          book_name: book_data[:title],
+          author: book_data[:authors].first,
+          thumbnail_url: book_data[:imageLinks][:smallThumbnail]
+        }
+      )
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  # Search endpoint
-  get '/search/:isbn', to: 'search#index'
+  # Search
+  post '/search', to: 'search#create'
+
+  # rest search endpoint
+  get '/search/:isbn', to: 'search#show'
 
   # App homepage
   root 'main#index'
